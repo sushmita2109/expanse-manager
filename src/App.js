7f9ee089fa2deaa4bd58f7527747b71dfc3c21e7
Sushmita <sushmitakumari363@gmail.com>
2022-12-28 11:37:52 +0530

    ReactRouter Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from './components/homePage';
+import { Nav } from './components/nav';
+import { Login } from './components/login';
+import { SignUp } from './components/signUp';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+     <div className="App">
+     <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path='login' element={<Login/>}/>
+      <Route path='signup' element={<SignUp/>}/>
+     </Routes>
+     </div>
+    </BrowserRouter>
   );
 }
 
