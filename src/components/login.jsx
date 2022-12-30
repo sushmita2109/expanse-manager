1f276b666086b62061f20f20ffd659b3acdf1e3a
Sushmita <sushmitakumari363@gmail.com>
2022-12-30 12:28:27 +0530

    login setup

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export const Login=()=>{
     return(
-        <div>
-            <h1>Login</h1>
+        <div className="container">
+            <main>
+                <div className="headtext">Login</div>
+                <form>
+                    <label htmlFor="email">Email Address</label>
+                    <input type="email" />
+                    <label htmlFor="pass">Password</label>
+                    <input type="password"/>
+                    <label>
+                    <input type="checkbox"></input>
+                    <span>I'm not a robot</span>
+                    </label>
+                    <button type="submit">Login</button>
+                </form>
+                <div>Forgot your password?</div>
+            </main>
         </div>
     )
 }
