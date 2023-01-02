9391ec66ccf62d257fe2de82bc84344614dfc1be
Sushmita <sushmitakumari363@gmail.com>
2023-01-02 10:06:29 +0530

    login styling

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-
+import './styles/login.css';
 export const Login=()=>{
     return(
         <div className="container">
-            <main>
-                <div className="headtext">Login</div>
-                <form>
-                    <label htmlFor="email">Email Address</label>
-                    <input type="email" />
-                    <label htmlFor="pass">Password</label>
-                    <input type="password"/>
-                    <label>
+            <main className="loginArea">
+                <div className="headText">Login</div>
+                <form className="loginForm">
+                    <label className="textLabel" htmlFor="email">Email Address</label>
+                    <input className="textInput" type="email" />
+                    <label className="textLabel" htmlFor="pass">Password</label>
+                    <input className="textInput" type="password"/>
+                    <label className="textLabel">
                     <input type="checkbox"></input>
                     <span>I'm not a robot</span>
                     </label>
-                    <button type="submit">Login</button>
+                    <button className="btn" type="submit">Login</button>
                 </form>
                 <div>Forgot your password?</div>
             </main>
